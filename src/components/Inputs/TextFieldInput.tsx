8c1a003041e6f2888474c55e26342eac03fca018
Jose Quesada <josesk8mqc@gmail.com>
2023-09-17 19:52:44 -0400

    Create TextFieldInput.

---
+++ src/components/Inputs/TextFieldInput.tsx
@@ -0,0 +1,68 @@
+import { 
+    Grid,
+    TextField,
+} from "@mui/material";
+import { TextFieldProps } from "@/interfaces";
+
+export const TextFieldInput: React.FC<TextFieldProps> = ({
+    xs='auto',
+    sm='auto',
+    md='auto',
+    lg='auto',
+    xl='auto',
+    gridItemZeroMinWidth=false,
+    id,
+    labelText,
+    name,
+    onChange,
+    autoComplete,
+    autoFocus=false,
+    color="primary",
+    disabled=false,
+    error,
+    fullWidth=true,
+    placeholder,
+    required=false,
+    value,
+    margin="none",
+    minRows,
+    maxRows,
+    rows=3,
+    size
+}) => {
+    return (
+        <Grid 
+            item={true}
+            xs={xs}
+            sm={sm}
+            md={md}
+            lg={lg}
+            xl={xl}
+            zeroMinWidth={gridItemZeroMinWidth}
+        >
+            <TextField
+                label={labelText}
+                id={id}
+                name={name}
+                onChange={onChange}
+                autoComplete={autoComplete}
+                autoFocus={autoFocus}
+                color={color}
+                disabled={disabled}
+                error={error.status}
+                placeholder={placeholder}
+                required={required}
+                value={value}
+                helperText={error.status ? error.message : null}
+                fullWidth={fullWidth}
+                type="text"
+                margin={margin}
+                multiline={true}
+                minRows={minRows}
+                maxRows={maxRows}
+                rows={rows}
+                size={size}
+            />
+        </Grid>
+    )
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,23 @@
 import { InputStandardProps } from "./InputStandardProps";
+import { SelectInputProps } from "./SelectInputProps";
+import { option as selectOption} from "./SelectInputProps";
+import { TextFieldProps } from "./TextField";
+import { InputConstructorProps } from "./InputConstructorProps"; 
+import { 
+    inputsArray,
+    valuesState,
+    validators, 
+    errors,
+} from "./useFormTypes";
 
 export type {
-    InputStandardProps
+    InputStandardProps,
+    SelectInputProps,
+    selectOption,
+    TextFieldProps,
+    InputConstructorProps,
+    inputsArray,
+    valuesState,
+    validators, 
+    errors,
 }
