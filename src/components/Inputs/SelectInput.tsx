60bae5ab6e80d5c1b9a9511bb206aadc7b8ee0c9
Jose Quesada <josesk8mqc@gmail.com>
2023-09-17 19:52:21 -0400

    create SelectInput.

---
+++ src/components/Inputs/SelectInput.tsx
@@ -0,0 +1,95 @@
+import { 
+    Grid,
+    InputLabel,
+    FormControl,
+    FormHelperText,
+    Select,
+    MenuItem,
+} from "@mui/material";
+import { 
+    SelectInputProps, 
+    selectOption 
+} from "@/interfaces";
+import { v4 as uuid } from "uuid";
+
+export const SelectInput: React.FC<SelectInputProps> = ({
+    xs='auto',
+    sm='auto',
+    md='auto',
+    lg='auto',
+    xl='auto',
+    gridItemZeroMinWidth=false,
+    id,
+    labelText,
+    name,
+    onSelect,
+    autoComplete,
+    autoFocus=false,
+    color="primary",
+    disabled=false,
+    error,
+    readOnly=false,
+    required=false,
+    value,
+    options=[],
+    autoWidth=false,
+    defaultOpen=false,
+    multiple=false,
+    size,
+}) => {
+    return (
+        <Grid 
+            item={true}
+            xs={xs}
+            sm={sm}
+            md={md}
+            lg={lg}
+            xl={xl}
+            zeroMinWidth={gridItemZeroMinWidth}
+        >
+            <FormControl fullWidth>
+                <InputLabel
+                    htmlFor={id}
+                    variant="outlined"
+                >
+                    {labelText}
+                </InputLabel>
+                <Select
+                    label={labelText}
+                    id={id}
+                    name={name}
+                    onChange={onSelect}
+                    autoComplete={autoComplete}
+                    autoFocus={autoFocus}
+                    color={color}
+                    disabled={disabled}
+                    error={error.status}
+                    readOnly={readOnly}
+                    required={required}
+                    value={value}
+                    variant="outlined"
+                    autoWidth={autoWidth}
+                    defaultOpen={defaultOpen}
+                    multiple={multiple}
+                    size={size}
+                >
+                    {options.map((option: selectOption) => (
+                        <MenuItem 
+                            value={option.value}
+                            key={uuid()}
+                        >
+                            {option.text}
+                        </MenuItem>
+                    ))}
+                </Select>
+                {error.status ?
+                    (
+                        <FormHelperText error={true}>
+                            {error.message}
+                        </FormHelperText>
+                    )
+                : null}
+            </FormControl>
+        </Grid>
+    )
+}
