a9734c22fec825522062b2c7ba63467af2355d85
Jose Quesada <josesk8mqc@gmail.com>
2023-10-01 19:29:30 -0400

    se agregaron inputs al index.ts.

--- src/components/Inputs/index.ts
+++ src/components/Inputs/index.ts
@@ -1,5 +1,9 @@
-import { InputStandard} from "./InputStandard";
+import { InputStandard } from "./InputStandard";
+import { SelectInput } from "./SelectInput";
+import { TextFieldInput } from "./TextFieldInput";
 
 export {
     InputStandard,
+    SelectInput,
+    TextFieldInput,
 }
