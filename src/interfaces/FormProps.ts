7b7705045ec8b99a496ca30cc3a3bbd5609d9ab9
Jose Quesada <josesk8mqc@gmail.com>
2023-10-04 17:36:30 -0400

    create Form component.

---
+++ src/interfaces/FormProps.ts
@@ -0,0 +1,30 @@
+import { ChangeEventHandler, FormEventHandler, ReactNode } from "react";
+import { 
+    ButtonBaseProps, 
+    alertState, 
+    errors, 
+    inputProps, 
+    inputsArray, 
+    valuesState
+} from "./index";
+import { SelectChangeEvent } from "@mui/material";
+
+export type FormProps = {
+    title?: string,
+    subheader?: string,
+    buttons?: ButtonBaseProps[],
+    inputs: inputProps[],
+    values: valuesState,
+    onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+    onSelect?: (event: SelectChangeEvent<any>, child: ReactNode) => void,
+    errors?: errors,
+    handleSubmit: any,
+    alert?: alertState,
+    handleCloseAlert: () => void,
+    styleClass?: string,
+    link?: {
+        sideText: string,
+        to: string,
+        text: string,
+    },
+}
