31b5e17ea4585cbbbc962b48f50cba68a8901990
Jose Quesada <josesk8mqc@gmail.com>
2023-10-13 20:13:04 -0400

    add footer to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Navbar } from '@/components/Navbar/Navbar'
 import { CategoriesMenu } from '@/components/CategoriesMenu/CategoriesMenu'
 import { LandingBanners } from '@/containers/LandingBanners/LandingBanners'
-import { Typography } from '@mui/material'
-import styles from './page.module.css'
-import Image from 'next/image'
 import { LandingBannersSide } from '@/containers/LandingBannersSide/LandingBannersSide'
+import { Footer } from '@/components/Footer/Footer'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
@@ -16,29 +15,10 @@ export default function Home() {
           <LandingBannersSide/>
         </aside>
         <section className={styles.content__main__container}>
-          <div className={styles.content__main__head}>
-            <Image 
-              src="/assets/logo.jpg" 
-              alt="logo" 
-              width="60" 
-              height="60" 
-              className={styles.head__logo__img}
-            />
-            <Typography 
-                fontFamily="Barlow Condensed Regular" 
-                fontSize="30px" 
-                color="#4D5E80" 
-                textAlign="center"
-            >
-                Tech Market
-            </Typography>
-          </div>
           <LandingBanners/>
         </section>
       </section>
-      <footer className={styles.footer__container}>
-        footer
-      </footer>
+      <Footer/>
     </section>
   )
 }
