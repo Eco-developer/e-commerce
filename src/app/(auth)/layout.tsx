32e6c79649dd082dff59eb727cd8a5d24ca8c644
Jose Quesada <josesk8mqc@gmail.com>
2023-10-31 20:41:31 -0400

    add rootlayout to auth.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,8 @@
+import { Inter } from 'next/font/google';
 import styles from "./layout.module.css";
+import '../globals.css';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function AuthLayout({
     children, 
@@ -6,17 +10,21 @@ export default function AuthLayout({
     children: React.ReactNode
   }) {
     return (
-        <main className={styles.auth__container}>
-            <span className={styles.sideimage__container}>
-                <img 
-                    src="/assets/authsideimage.jpg" 
-                    alt="sideimage" 
-                    className={styles.sideimage}
-                />
-            </span>
-            <span className={styles.auth__content}>
-                {children}
-            </span>
-        </main>
+        <html lang="en">
+            <body className={inter.className}>
+                <main className={styles.auth__container}>
+                    <span className={styles.sideimage__container}>
+                        <img 
+                            src="/assets/authsideimage.jpg" 
+                            alt="sideimage" 
+                            className={styles.sideimage}
+                        />
+                    </span>
+                    <span className={styles.auth__content}>
+                        {children}
+                    </span>
+                </main>
+            </body>
+        </html> 
     )
   }
