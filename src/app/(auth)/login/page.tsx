27b72395c3753047c6d3fdf6ebdc7e6ca45942c3
Jose Quesada <josesk8mqc@gmail.com>
2023-11-07 20:53:45 -0400

    remove login container from login page.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,7 @@
-import styles from "./page.module.css"
 import { LoginForm } from "@/containers/LoginForm/LoginForm";
 
 export default function SignIn() {
   return (
-    <div className={styles.login_container}>
-      <LoginForm/>
-    </div>
+    <LoginForm/>
   )
 }
