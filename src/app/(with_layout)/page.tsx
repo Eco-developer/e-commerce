26624fc279e274db77ae11cfb936cc9a8a713a73
Jose Quesada <josesk8mqc@gmail.com>
2023-11-10 20:53:06 -0400

    change main content's layout.

--- src/app/(with_layout)/page.tsx
+++ src/app/(with_layout)/page.tsx
@@ -1,6 +1,5 @@
 import { CategoriesMenu } from '@/components/CategoriesMenu/CategoriesMenu'
 import { LandingBanners } from '@/containers/LandingBanners/LandingBanners'
-import { LandingBannersSide } from '@/containers/LandingBannersSide/LandingBannersSide'
 import styles from './page.module.css'
 import { API_TARGET } from '@/const/baseUrl'
 
@@ -33,15 +32,14 @@ export default async function Home() {
   } = await getData();
   return (
     <section className={styles.content__container}>
-      <aside className={styles.content__side__container}>
+      <section className={styles.content__side__container}>
         { stores.length ?
           <CategoriesMenu 
             stores={stores[0].locations}
             categories={categories.slots["1100"]?.content?.taxonomy_nodes} 
           />
         : null  } 
-        <LandingBannersSide/>
-      </aside>
+      </section>
       <section className={styles.content__main__container}>
         <LandingBanners/>
       </section>
