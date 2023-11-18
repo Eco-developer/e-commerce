8fb31db3aea1d2d35aa67172a0b3e428ddd765f3
Jose Quesada <josesk8mqc@gmail.com>
2023-11-17 20:46:08 -0400

    add contact form and contact info to contact page.

--- src/app/(with_layout)/contact/page.tsx
+++ src/app/(with_layout)/contact/page.tsx
@@ -1,7 +1,13 @@
+import { ContactInfo } from "@/containers/ContactInfo/ContactInfo";
+import { ContactForm } from "@/containers/ContactForm/ContactForm";
+import styles from "./page.module.css";
+import { infoItems } from "@/const/infoItems";
+
 export default function Contact() {
     return (
-      <div>
-        Contact
-      </div>
+      <section className={styles.contact__container}>
+        <ContactInfo infoItems={infoItems}/> 
+        <ContactForm/>
+      </section>
     )
 }
